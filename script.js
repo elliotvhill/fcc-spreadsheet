e87ff73a79a51225b80a83863414751c5700fd80
Elliot <ellthrowaway2@icloud.com>
2024-07-05 18:25:36 -0400

    building spreadsheet functions evaluation logic

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const spreadsheetFunctions = {
+    sum,
+    average,
+    median,
+};
+
+const range = (start, end) =>
+    Array(end - start + 1)
+        .fill(start)
+        .map((element, index) => element + index);
+
+const charRange = (start, end) =>
+    range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+        String.fromCharCode(code)
+    );
+
+const evalFormula = (x, cells) => {
+    const idToText = (id) => cells.find((cell) => cell.id === id).value;
+};
+
 window.onload = () => {
     const container = document.getElementById("container");
+
     const createLabel = (name) => {
         const label = document.createElement("div");
         label.className = "label";
@@ -15,17 +36,26 @@ window.onload = () => {
             input.type = "text";
             input.id = letter + number;
             input.ariaLabel = letter + number;
+            input.onchange = update;
             container.appendChild(input);
         });
     });
 };
 
-const range = (start, end) =>
-    Array(end - start + 1)
-        .fill(start)
-        .map((element, index) => element + index);
+const sum = (nums) => nums.reduce((acc, currVal) => acc + currVal, 0);
+const isEven = (num) => (num % 2 === 0 ? true : false);
+const average = (nums) => sum(nums) / nums.length;
+const median = (nums) => {
+    const sorted = nums.slice().sort((a, b) => a - b);
+    const length = sorted.length;
+    const middle = length / 2 - 1;
+    return isEven(length)
+        ? average([sorted[middle], sorted[middle + 1]])
+        : sorted[Math.ceil(middle)];
+};
 
-const charRange = (start, end) =>
-    range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
-        String.fromCharCode(code)
-    );
+const update = (event) => {
+    const element = event.target;
+    const value = element.value.replace(/\s/g, "");
+    if (!value.includes(element.id) && value[0] === "=") {};
+};
