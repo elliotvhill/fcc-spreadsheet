9affbff2ca59cb4e70554e43f0a0109492b52317
Elliot <ellthrowaway2@icloud.com>
2024-07-05 18:27:22 -0400

    fixed code order oversight

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const sum = (nums) => nums.reduce((acc, currVal) => acc + currVal, 0);
+const isEven = (num) => (num % 2 === 0 ? true : false);
+const average = (nums) => sum(nums) / nums.length;
+const median = (nums) => {
+    const sorted = nums.slice().sort((a, b) => a - b);
+    const length = sorted.length;
+    const middle = length / 2 - 1;
+    return isEven(length)
+        ? average([sorted[middle], sorted[middle + 1]])
+        : sorted[Math.ceil(middle)];
+};
+
 const spreadsheetFunctions = {
     sum,
     average,
@@ -42,17 +54,6 @@ window.onload = () => {
     });
 };
 
-const sum = (nums) => nums.reduce((acc, currVal) => acc + currVal, 0);
-const isEven = (num) => (num % 2 === 0 ? true : false);
-const average = (nums) => sum(nums) / nums.length;
-const median = (nums) => {
-    const sorted = nums.slice().sort((a, b) => a - b);
-    const length = sorted.length;
-    const middle = length / 2 - 1;
-    return isEven(length)
-        ? average([sorted[middle], sorted[middle + 1]])
-        : sorted[Math.ceil(middle)];
-};
 
 const update = (event) => {
     const element = event.target;
